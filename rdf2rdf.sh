b1176b8b3d267d4495ed066770dfb68f534b1aec
architolk <msj@xs4all.nl>
2019-01-18 09:34:04 +0100

    First commit

---
+++ rdf2rdf.sh
@@ -0,0 +1,1 @@
+java -jar target/rdf2rdf-*-jar-with-dependencies.jar data/input.xml data/output.json
---
+++ src/main/java/nl/architolk/rdf2rdf/Convert.java
@@ -0,0 +1,34 @@
+package nl.architolk.rdf2rdf;
+
+import java.io.FileOutputStream;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.riot.RDFDataMgr;
+import org.apache.jena.riot.RDFFormat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Convert {
+
+  private static final Logger LOG = LoggerFactory.getLogger(Convert.class);
+
+  public static void main(String[] args) {
+
+    if (args.length == 2) {
+
+      LOG.info("Starting conversion");
+      LOG.info("Input file: {}",args[0]);
+      LOG.info("Ouput file: {}",args[1]);
+
+      try {
+        Model model = RDFDataMgr.loadModel(args[0]);
+        RDFDataMgr.write(new FileOutputStream(args[1]),model, RDFFormat.JSONLD_COMPACT_PRETTY);
+        LOG.info("Done!");
+      }
+      catch (Exception e) {
+        LOG.error(e.getMessage(),e);
+      }
+    } else {
+      LOG.info("Usage: archimate2rdf <input.xml> <output.xml>");
+    }
+  }
+}
